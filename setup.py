1e83534684083b1a228c563ee9c85138701156b6
Michell Stuttgart <michellstut@gmail.com>
2017-01-03 11:49:32 -0200

    [FIX] Update tags on setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,48 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Jadson Bonfim Ribeiro <jadsonbr@outlook.com.br>
+#
 
-files = ["jasperstarter/*", "test/*"]
+from setuptools import setup, find_packages
+import os
+import re
+
+
+def get_version(package):
+    """
+    Based in https://github.com/tomchristie/django-rest-framework/blob/
+    971578ca345c3d3bae7fd93b87c41d43483b6f05/setup.py
+    :param package Package name
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 setup(
     name='pyreportjasper',
-    version='0.1.3',
+    version=get_version('pyjasper'),
     url='https://github.com/jadsonbr/pyreport',
     license='MIT License',
     author='Jadson Bonfim Ribeiro',
     author_email='jadsonbr@outlook.com.br',
-    keywords='report jasper python relatorio',
-    description=u'This package aims to be a solution to compile and process '
-                u'JasperReports (.jrxml & .jasper files).',
+    keywords='report jasper python',
+    description='This package aims to be a solution to compile and process '
+                'JasperReports (.jrxml & .jasper files).',
     packages=find_packages(),
     install_requires=[
     ],
     test_suite='test',
     package_data={
-        'package': files,
+        'package': ['jasperstarter/*'],
     },
     include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7 3.3 3.4 3.5',
+    ],
 )
