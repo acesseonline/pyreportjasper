0023547b3bac2b28b5266a723b63d87e4fa58fe7
jadsonbr <jadsonbr@outlook.com.br>
2017-01-20 17:05:45 -0300

    Updated version of jasperreport for 6.4.1 with integrated support of COURIER NEW font especially for linux, and jasperstarter for 3.2.1. Added more tests and outdated libraries excluded.

--- test/jasperpy_test.py
+++ test/jasperpy_test.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # The MIT License (MIT)
 #
+# Copyright (c) 2017 Jadson Bonfim Ribeiro <jadsonbr@outlook.com.br>
 # Copyright (c) 2017 Michell Stuttgart <michellstut@gmail.com>
 #
 
+import os
 from unittest import TestCase
 from pyjasper.jasperpy import JasperPy
 
@@ -12,18 +14,26 @@ class TestJasperPy(TestCase):
 
     def setUp(self):
         self.input_file = 'examples/hello_world.jrxml'
+        self.output_dir = 'examples/output/'
         self.jasper = JasperPy()
 
     def test_compile(self):
+        """
+        Test of compile method, of class JasperPy.
+        """
         self.assertRaises(NameError, self.jasper.compile, False)
-        self.assertEqual(self.jasper.compile(self.input_file), 0)
+        self.assertEqual(self.jasper.compile(self.input_file, self.output_dir), 0)
 
     def test_process(self):
+        """
+        Test of process method, of class JasperPy.
+        """
         self.assertRaises(NameError, self.jasper.process, False)
 
         # test invalid format input. Must be list ['pdf']
         kwargs = {
             'input_file': self.input_file,
+            'output_file': self.output_dir,
             'format_list': 'pdf'
         }
         self.assertRaises(NameError, self.jasper.process, **kwargs)
@@ -38,16 +48,20 @@ class TestJasperPy(TestCase):
         # test invalid report format
         kwargs = {
             'input_file': self.input_file,
+            'output_file': self.output_dir,
             'format_list': ['mp3']
         }
         self.assertRaises(NameError, self.jasper.process, **kwargs)
 
         # test
         self.assertEqual(
-            self.jasper.process(self.input_file,
+            self.jasper.process(self.input_file, self.output_dir,
                                 format_list=['pdf', 'odt', 'xls']), 0)
 
     def test_list_parameters(self):
+        """
+        Test of parameters list method, of class JasperPy.
+        """
         self.input_file = 'examples/hello_world_params.jrxml'
         self.assertRaises(NameError, self.jasper.list_parameters, False)
         self.assertEqual(self.jasper.list_parameters(self.input_file),
@@ -61,7 +75,24 @@ class TestJasperPy(TestCase):
                          })
 
     def test_execute(self):
+        """
+        Test of execute method, of class JasperPy.
+        """
         self.assertEqual(self.jasper.execute(), 0)
 
         self.jasper.path_executable = ''
         self.assertRaises(NameError, self.jasper.execute, False)
+
+    def test_compileToFileJasperreportsFunctions(self):
+        """
+        This report uses functions with dependency to jasperreports-functions
+        """
+        self.input_file = 'examples/charactersetTestWithStudioBuiltinFunctions.jrxml'
+        self.assertEqual(self.jasper.compile(self.input_file, self.output_dir), 0)
+
+    def test_compileToFileJavaScript(self):
+        """
+        This report uses functions with dependency to jasperreports-functions
+        """
+        self.input_file = 'examples/charactersetTestWithJavaScript.jrxml'
+        self.assertEqual(self.jasper.compile(self.input_file, self.output_dir), 0)
