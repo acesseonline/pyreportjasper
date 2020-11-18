44cf4fcf49e0f65df7f83be400a57c442bf19008
Jadson Ribeiro <contato@jadsonbr.com.br>
2020-11-18 17:07:57 -0300

    Change travis java set default

--- travisci/java_install.sh
+++ travisci/java_install.sh
@@ -5,17 +5,15 @@ echo ">>>>> Current JAVA_HOME: $JAVA_HOME <<<<<"
 echo ">>>>> Current java -version: <<<<<"
 java -version
 
-# install Java 8
+# install Java 15
 echo ">>>>> Add repository <<<<<"
 sudo add-apt-repository ppa:linuxuprising/java
 echo ">>>>> Update repository <<<<<"
 sudo apt-get -qq update
 echo ">>>>> Install jdk 15 <<<<<"
-sudo apt install -y oracle-java15-installer
-
-echo ">>>>> Accept licence jdk <<<<<"
+sudo apt install -y oracle-java15-set-default
 echo oracle-java15-installer shared/accepted-oracle-license-v1-2 select true | sudo /usr/bin/debconf-set-selections
-echo oracle-java15-installer shared/accepted-oracle-licence-v1-2 boolean true | sudo /usr/bin/debconf-set-selections
+
 
 echo "*********************************"
 java -version
