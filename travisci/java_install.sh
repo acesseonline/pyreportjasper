d2ffb86668bdcc2338bb3455619805dee77fcebd
Jadson Ribeiro <contato@jadsonbr.com.br>
2020-11-18 10:29:55 -0300

    Remove update alternative in script installation java

--- travisci/java_install.sh
+++ travisci/java_install.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 # show current JAVA_HOME and java version
-echo "Current JAVA_HOME: $JAVA_HOME"
-echo "Current java -version:"
+echo ">>>>> Current JAVA_HOME: $JAVA_HOME <<<<<"
+echo ">>>>> Current java -version: <<<<<"
 java -version
 
 # install Java 8
-echo "Add repository"
+echo ">>>>> Add repository <<<<<"
 sudo add-apt-repository -y ppa:openjdk-r/ppa
-echo "Update repository"
+echo ">>>>> Update repository <<<<<"
 sudo apt-get -qq update
-echo "Install open jdk 11"
+echo ">>>>> Install open jdk 11 <<<<<"
 sudo apt-get install -y openjdk-11-jdk
-echo "List alternatives update java"
-sudo update-alternatives --config java
 
 # change JAVA_HOME to Java 11
-echo "Set variable JAVA_HOME"
+echo ">>>>> Set variable JAVA_HOME <<<<<"
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
