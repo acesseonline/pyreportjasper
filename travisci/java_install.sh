1756d38b2743c4c1abbdeebd0fb05f7a475151a8
Jadson Ribeiro <contato@jadsonbr.com.br>
2020-11-18 09:58:06 -0300

    Change setup travis and add script install java 11

--- travisci/java_install.sh
+++ travisci/java_install.sh
@@ -6,10 +6,15 @@ echo "Current java -version:"
 java -version
 
 # install Java 8
+echo "Add repository"
 sudo add-apt-repository -y ppa:openjdk-r/ppa
+echo "Update repository"
 sudo apt-get -qq update
-sudo apt-get install -y openjdk-11-jdk --no-install-recommends
+echo "Install open jdk 11"
+sudo apt-get install -y openjdk-11-jdk
+echo "List alternatives update java"
 sudo update-alternatives --config java
 
 # change JAVA_HOME to Java 11
+echo "Set variable JAVA_HOME"
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
